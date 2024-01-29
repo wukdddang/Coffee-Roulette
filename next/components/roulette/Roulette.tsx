72152859b6bda14f6b377eada015ce7f21278249
wukdddang <dnr8874@naver.com>
2024-01-29 21:18:24 +0900

    Feat: 기존 프로젝트 작업 내용 반영

--- next/components/roulette/Roulette.tsx
+++ next/components/roulette/Roulette.tsx
@@ -49,22 +49,27 @@ export default function Roulette() {
     const targetModal = document.querySelector("#target-modal") as HTMLElement;
 
     if (mode === "hard") {
-      await fetch(`/api/user/increase/${winnerName}`).then((res) => {
+      await fetch(
+        `${process.env.NEXT_PUBLIC_BASEPATH}/api/user/increase/${winnerName}`
+      ).then((res) => {
         if (res.ok) {
           console.log("성공");
         } else {
           console.log("실패");
         }
       });
-      await fetch(`/api/user/increase/participation`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          participants,
-        }),
-      });
+      await fetch(
+        `${process.env.NEXT_PUBLIC_BASEPATH}/api/user/increase/participation`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            participants,
+          }),
+        }
+      );
     }
 
     const winnerHtml = document.querySelector(
@@ -73,7 +78,7 @@ export default function Roulette() {
 
     winnerHtml.innerHTML = "";
     winnerHtml.innerHTML = `
-      <img src="/imgs/congratulation.png" class="target-modal-body-background"/>
+      <img src="${process.env.NEXT_PUBLIC_BASEPATH}/imgs/congratulation.png" class="target-modal-body-background"/>
       <div class="target-modal-body-text font-weight-bold text-center">${winnerName}님! <br/> 커피 감사합니다! ☕</div>
     `;
 
