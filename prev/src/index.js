194267f24cb74932de6c72393252b706e9de3df8
wukdddang <dnr8874@naver.com>
2024-02-07 08:57:34 +0900

    Chore: prev 버전 포트 변경 + 도커 파일 추가

--- prev/src/index.js
+++ prev/src/index.js
@@ -250,6 +250,6 @@ app.get("/admin/user/delete/:id", async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log("Server started on port 3001");
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
 });
